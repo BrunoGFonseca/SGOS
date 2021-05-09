5f3ed4420616c54c9544e9d82d0d0f4b8cb68167
BrunoGFonseca <brunogabrielfonseca@hotmail.com>
2021-05-09 11:03:22 -0300

    Projeto criado

---
+++ src/test/java/com/brunofonseca/SGOS/SgosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunofonseca.SGOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
