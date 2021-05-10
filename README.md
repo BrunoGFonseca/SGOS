3dc09e499b7e5f8e70cf9130e745399429c564c6
BrunoGFonseca <brunogabrielfonseca@hotmail.com>
2021-05-09 23:10:57 -0300

    Criando operação de instanciacao

--- README.md
+++ README.md
@@ -17,3 +17,4 @@ Esse repositório está armazenando o SGOS, um sistema para controle de ordens d
 - **Testando duas primeiras classes de dominio**: Ajuste nas classes CategoriaProdutoResource e CategoriaServicoResource, mocando categorias para testes. 
 - **Banco de dados H2 e criação automatica da base de dados**: Configuração do H2 e a criação automatica da base de dados a partir das classes de dominio.
 - **Criando Repository e Service para CategoriaProduto e CategoriaServico**: Criando classes de Repository e de Service para os produtos e para os serviços da aplicação.
+- **Criando operação de instanciacao**: Criado as operações para instanciar categorias de servico e categorias de produtos.
--- src/main/java/com/brunofonseca/SGOS/SgosApplication.java
+++ src/main/java/com/brunofonseca/SGOS/SgosApplication.java
@@ -1,13 +1,40 @@
 package com.brunofonseca.SGOS;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.brunofonseca.SGOS.domain.CategoriaProduto;
+import com.brunofonseca.SGOS.domain.CategoriaServico;
+import com.brunofonseca.SGOS.repositories.CategoriaProdutoRepository;
+import com.brunofonseca.SGOS.repositories.CategoriaServicoRepository;
+
 @SpringBootApplication
-public class SgosApplication {
+public class SgosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaProdutoRepository categoriaProdutoRepository;
+	
+	@Autowired
+	private CategoriaServicoRepository categoriaServicoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		CategoriaProduto catProd1 = new CategoriaProduto(null, "ProdutoMotor");
+		CategoriaProduto catProd2 = new CategoriaProduto(null, "ProdutoSuspensao");
+		
+		CategoriaServico catServ1 = new CategoriaServico(null, "ServicoMotor");
+		CategoriaServico catServ2 = new CategoriaServico(null, "ServicoSuspensao");
+
+		categoriaProdutoRepository.saveAll(Arrays.asList(catProd1, catProd2));
+		categoriaServicoRepository.saveAll(Arrays.asList(catServ1, catServ2));
+	}
 }
