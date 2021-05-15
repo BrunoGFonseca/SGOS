dfa2c618e23787d8bf429830fcfa5c125bdcdabb
BrunoGFonseca <brunogabrielfonseca@hotmail.com>
2021-05-15 10:01:20 -0300

    Criando método PUT, DELETE, GET para classe cliente

--- README.md
+++ README.md
@@ -38,3 +38,4 @@ Para um melhor desenvolvimento, eu decidi que iria montar o BackEnd da aplicaç�
 - **Paginação com parametros opcionais**: É possível paginar as categorias. <br /><br />
 - **Validação sintática usando o BEAN VALIDATION**: Adicionando validações sem aceso a dados nos nomes das categorias. <br /><br />
 - **Retorno da validação personalizado**: Criando um padrão para o retorno dos erros. <br /><br />
+- **Criando método PUT, DELETE, GET para classe cliente**: Desenvolvendo CRUD para classe de clientes. <br /><br />
--- src/main/java/com/brunofonseca/SGOS/services/ClienteService.java
+++ src/main/java/com/brunofonseca/SGOS/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.brunofonseca.SGOS.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brunofonseca.SGOS.domain.Cliente;
+import com.brunofonseca.SGOS.dto.ClienteDTO;
 import com.brunofonseca.SGOS.repositories.ClienteRepository;
+import com.brunofonseca.SGOS.services.exceptions.DataIntegrityException;
 import com.brunofonseca.SGOS.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +26,43 @@ public class ClienteService {
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll(){
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageResquest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageResquest);
+	}
+	
+	//public Cliente insert(Cliente obj) {
+	//	obj.setId(null);
+	//	return clienteRepository.save(obj);
+	//}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir pois existem entidades relacionadas.");
+		}
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}	
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
